0ab36de62045fed38e2dadf69ceff88e9ecc186d
Mikhelmariya <mikhela65@gmail.com>
2023-05-19 23:34:47 +0530

    column widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,65 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter/src/widgets/navigator.dart';
+
 void main() {
-  runApp(FlutterBasics());
+  runApp(MyApp());
 }
 
-class FlutterBasics extends StatelessWidget {
-  const FlutterBasics({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: Center(
-            child: RichText(
-          text: TextSpan(
-            text: 'Hello ',
-            style: DefaultTextStyle.of(context).style,
-            children: <TextSpan>[
-              TextSpan(
-                  text: 'bold', style: TextStyle(fontWeight: FontWeight.bold)),
-              TextSpan(text: ' world!', style: TextStyle(color: Colors.red)),
-            ],
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Text(
+                  "I'm the first child of column widget",
+                  style: TextStyle(
+                      color: Colors.yellow,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "I'm the second child of column widget",
+                  style: TextStyle(
+                      color: Colors.yellow,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 100,
+                  width: 400,
+                  color: Colors.red,
+                  child: Center(
+                    child: Text("I'm a Container and 3rd child of column",
+                        style: TextStyle(
+                            color: Colors.yellow,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold)),
+                  ),
+                ),
+              ],
+            ),
           ),
-        )),
+        ),
       ),
     );
   }
